97e5fc1d15bc2a0fecb28d827ce054af10d90805
edwin-collab-coder <delreal678@gmail.com>
2026-03-26 16:27:34 -0700

    768 hover pendient

--- src/js/script.js
+++ src/js/script.js
@@ -67,4 +67,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  const collapseElement = document.getElementById('collapseExample');
+
+collapseElement.addEventListener('shown.bs.collapse', function () {
+  document.body.classList.add('no-scroll');
+});
+
+collapseElement.addEventListener('hidden.bs.collapse', function () {
+  document.body.classList.remove('no-scroll');
+});
+
+const btnEnviar = document.getElementById('btnEnviar');
+
+btnEnviar.addEventListener('click', function () {
+  const bsCollapse = bootstrap.Collapse.getInstance(collapseElement);
+  bsCollapse.hide(); // esto quita automáticamente la clase "show"
+});
+
 });
