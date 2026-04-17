c208e935efcd88014d9bc922dae42fc6a3290ee2
Edwin <mt300123@utnay.edu.mx>
2026-04-16 17:21:02 -0700

    Style-hover-678

--- js-src/03-ScrollFunction.js
+++ js-src/03-ScrollFunction.js
@@ -9,8 +9,12 @@ window.addEventListener('scroll', () => {
   if (currentScroll > 50) {
     topbar.classList.add('hide-topbar');
     navbar.style.top = "0";
+    navbar.style.position = "fixed";
+    document.body.classList.add('navbar-active');
   } else {
+    navbar.style.position = "unset";
     topbar.classList.remove('hide-topbar');
+    document.body.classList.remove('navbar-active');
     // Aplica solo si el ancho >= 1024px
     if (windowWidth >= 1024) {
       navbar.style.top = "39px";
